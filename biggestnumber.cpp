4ecfba4d350eca8fc08fd1232a8f5633b4574f10
mitcaizi <jling1@mail.csuchico.edu>
2018-02-05 18:20:21 -0800

    Description of what this version does

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -9,11 +9,20 @@ int main()
   int b;
   cout<<"Please enter a whole number:\n";
   cin>>a;
-
+  
   cout<<"Please enter another whole number:\n";
   cin>>b;
+  if (a<=b)
+  {
 
+   
+ 
   cout<<"Of those two numbers, the biggest is: ";
+  cout<<b;
+  }
+  else
+  {
+  }
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
