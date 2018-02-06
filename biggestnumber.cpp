2a0152d7557bb746e9d7bad89608b76eb6e9c886
mitcaizi <jling1@mail.csuchico.edu>
2018-02-05 17:43:47 -0800

    Description of what this version does

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,14 +1,17 @@
-//Author:
+//Author:Jingwen Ling
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int a; 
+  int b;
   cout<<"Please enter a whole number:\n";
-  
+  cin>>a;
+
   cout<<"Please enter another whole number:\n";
+  cin>>b;
 
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
